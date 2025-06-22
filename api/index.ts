dc3e8f773a44fe7dd1a17616b037f8443f889c5b
Gustavo Mena <gustavocanulmena@gmail.com>
2025-06-22 01:57:14 -0600

    delete changes to vercel

--- api/index.ts
+++
@@ -1,9 +0,0 @@
-import { AppRoutes } from '../src/presentation/routes';
-import express from 'express';
-
-const app = express();
-
-app.use(express.json());
-app.use(AppRoutes.routes);
-
-export default app;
