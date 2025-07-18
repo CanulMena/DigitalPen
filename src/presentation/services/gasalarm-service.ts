e7090df904d18f9f427024b0920907e12072b45a
Gustavo Mena <gustavocanulmena@gmail.com>
2025-07-18 02:33:35 -0600

    asdfsadf

--- src/presentation/services/gasalarm-service.ts
+++ src/presentation/services/gasalarm-service.ts
@@ -1,30 +1,28 @@
-import { PrismaClient } from "@prisma/client";
+
 import { GasAlarmEntity } from "../../domain/entities/gas-alarm";
 import { RegisterGasAlarm } from "../../domain/dtos/register-gas-alarm.dto";
+import { prisma } from "../../config/prisma";
 
 export class GasAlarmService {
-
-  public readonly prisma = new PrismaClient().alarmaGas; // Access to the database
-
   public async register(registerGasAlarmDto: RegisterGasAlarm): Promise<GasAlarmEntity> {
-    const gasAlarmRegisteredData = await this.prisma.create({
+    const gasAlarmRegisteredData = await prisma.alarmaGas.create({
       data: {
         fecha: new Date(registerGasAlarmDto.date).toISOString(),
         gas: registerGasAlarmDto.gasLevel,
-        usuarioId: registerGasAlarmDto.userId, // Assuming userId is part of the DTO
+        usuarioId: registerGasAlarmDto.userId,
       },
     });
 
-    const gasAlarmEntity = GasAlarmEntity.fromJson(gasAlarmRegisteredData);
-    return gasAlarmEntity;
+    return GasAlarmEntity.fromJson(gasAlarmRegisteredData);
   }
 
-  public async  getAll(): Promise<GasAlarmEntity[]> {
-    return await this.prisma.findMany()
-      .then((gasAlarms) => gasAlarms.map(GasAlarmEntity.fromJson))
-      .catch((error) => {
-        console.error("Error fetching gas alarms:", error);
-        throw error;
-      });
+  public async getAll(): Promise<GasAlarmEntity[]> {
+    try {
+      const gasAlarms = await prisma.alarmaGas.findMany();
+      return gasAlarms.map(GasAlarmEntity.fromJson);
+    } catch (error) {
+      console.error("Error fetching gas alarms:", error);
+      throw error;
+    }
   }
 }
--- src/presentation/services/iotlog-service.ts
+++ src/presentation/services/iotlog-service.ts
@@ -1,17 +1,13 @@
-import { PrismaClient } from "@prisma/client";
+
 import { IoTLogEntity } from "../../domain/entities/iotlog";
 import { CustomError } from "../../domain/errors/custom-error";
 import { RegisterIoTLogDto } from "../../domain/dtos/register-iotlog.dto";
+import { prisma } from "../../config/prisma";
 
-export class IoTLogService { //para mi los servicios es un patron command (tiene logica de negocio) pero con las peticiones a la API (acceso de datos) acoplado.
-  constructor(){}
-  //al hacer public una propiedad, quiere decir que es un propiedad de instancia (se puede acceder desde otros metodos de la clase pero se necessita instanciar) - al ser static ess todo lo contrario.
-  public readonly prisma = new PrismaClient().registrosIoT; //acceso a la base de datos
-
-  public async register( registerIotlogDto: RegisterIoTLogDto ): Promise<IoTLogEntity> {
+export class IoTLogService {
+  public async register(registerIotlogDto: RegisterIoTLogDto): Promise<IoTLogEntity> {
     try {
-
-      const iotLogCreated = await this.prisma.create({
+      const iotLogCreated = await prisma.registrosIoT.create({
         data: {
           humedad: registerIotlogDto.humidity,
           temperatura: registerIotlogDto.temperature,
@@ -23,7 +19,6 @@ export class IoTLogService { //para mi los servicios es un patron command (tiene
       if (!iotLogCreated) throw CustomError.internalServer('Registro IoT no creado');
 
       return IoTLogEntity.fromJson(iotLogCreated);
-      
     } catch (error) {
       console.error("Error registering IoT log:", error);
       throw error;
@@ -32,12 +27,11 @@ export class IoTLogService { //para mi los servicios es un patron command (tiene
 
   public async getAll(): Promise<IoTLogEntity[]> {
     try {
-      const iotLogs = await this.prisma.findMany();
+      const iotLogs = await prisma.registrosIoT.findMany();
       return iotLogs.map(IoTLogEntity.fromJson);
     } catch (error) {
       console.error("Error fetching IoT logs:", error);
       throw error;
     }
   }
-
 }
